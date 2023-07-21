2a57f7309bcf8762d01d5d7c59630a6cc81ddb42
andika <yuda.yokesen@gmail.com>
2023-07-21 14:46:48 +0700

    Update Berita API

--- app/Models/Berita.php
+++ app/Models/Berita.php
@@ -4,10 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Berita extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $guarded = ['id', 'deleted_at', 'updated_at', 'created_at'];
 
